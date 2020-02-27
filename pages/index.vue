9ed356c0420a4c0513b8edba7317584203e7620f
yayanheeh <tokoxp58@gmail.com>
2020-02-27 11:57:43 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+    <site-hero :title="Umroh murah" :subtitle="Umroh murah surabaya sidoarjo gresik" :image="featureImage">
       <button
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
